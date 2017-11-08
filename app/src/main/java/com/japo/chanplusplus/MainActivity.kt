454f0a2c873eb8df844898f2ade0ba6bf349df91
Jairo <jairoandre@gmail.com>
2017-11-08 01:36:45 -0200

    First commit

---
+++ app/src/main/java/com/japo/chanplusplus/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.japo.chanplusplus
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+    }
+}
